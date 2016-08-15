cdb88e105077548f66e26c850b65323d70253a49
Molly McGlone and Niki Garner <molly@>
2016-08-15 13:42:30 -0700

    connect announcement model to database

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
-var announcements = [{
-  id: 1,
-  name: "Bob",
-  message: "Don't feed the skunks"
-}];
-
 export default Ember.Route.extend({
   model() {
-    return announcements;
+    return this.store.findAll('announcement');
   },
 });
